c69b04d060a11dabd580a257c2426b2626dbfd55
NoSharp <33969918+NoSharp@users.noreply.github.com>
2020-07-29 02:04:48 +0100

    Optimisation
    A bunch of micro-optimizations.

--- lua/entities/medic_npc/cl_init.lua
+++ lua/entities/medic_npc/cl_init.lua
@@ -29,21 +29,32 @@ else
 	lang = {health = "Health:",armor = "Armor:",overhead = "Medic"}
 end
 
+local localPly = LocalPlayer()
+
+hook.Add("Think", "medic_npc_localply", function()
+	localPly = LocalPlayer()
+hook.Remove("Think", "medic_npc_localply")
+end)
+
+
+local white = color_white
+local black = Color(0,0,0)
+local mainOffset = Vector(0, 0, 80)
 function ENT:Draw()
 	self.Entity:DrawModel()
 	local mypos = self:GetPos()
-	if (LocalPlayer():GetPos():Distance(mypos) >= 1000) then return end
-	local offset = Vector(0, 0, 80)
+	if (localPly:GetPos():DistToSqr(mypos) >= 2000) then return end
+	local offset = mainOffset
 	local pos = mypos + offset
-	local ang = (LocalPlayer():EyePos() - pos):Angle()
+	local ang = (localPly:EyePos() - pos):Angle()
 	ang.p = 0
 	ang:RotateAroundAxis(ang:Right(), 90)
 	ang:RotateAroundAxis(ang:Up(), 90)
 	ang:RotateAroundAxis(ang:Forward(), 180)
 	cam.Start3D2D(pos,ang,0.04)
-		draw.RoundedBox(6,-250-2,0-2,500+4,150+4,Color(255,255,255))
-		draw.RoundedBox(6,-250,0,500,150,Color(0,0,0))
-		draw.SimpleText(lang.overhead,"medic_topText",0,75,Color(255,255,255),1,1)
+		draw.RoundedBox(6,-250-2,0-2,500+4,150+4,white)
+		draw.RoundedBox(6,-250,0,500,150,black)
+		draw.SimpleText(lang.overhead,"medic_topText",0,75,white,1,1)
 	cam.End3D2D()
 end
 
